246bd546eae7900256c709e07c0cd2634497ce4d
Mochammad Ridwan <rmz3t.developers@gmail.com>
2026-06-10 13:23:06 +0000

    updating source files

---
+++ src/src/main/java/io/rmz3t/fabric/scriptloader/Z3ScriptLoaderDataGenerator.java
@@ -0,0 +1,11 @@
+package io.rmz3t.fabric.scriptloader;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Z3ScriptLoaderDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
